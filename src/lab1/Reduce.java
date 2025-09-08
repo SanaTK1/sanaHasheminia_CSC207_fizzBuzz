b1cace4ae542f62f41172ebc562edc9c7ebb0b72
SanaTK1 <sana.hasheminia@gmail.com>
2025-09-08 12:43:45 -0400

    Fixed naming mistake in Reduce class

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -2,10 +2,10 @@ package lab1;
 
 public class Reduce {
     public static void main(String[] args) {
-        System.out.println(reducer(100));
+        System.out.println(reduce(100));
     }
 
-    private static int reducer(int n) {
+    private static int reduce(int n) {
         int counter = 0;
         while (n > 0){
             if(n%2 == 0)
