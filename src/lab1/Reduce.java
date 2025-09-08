88176cca1ace8dc29f234ce39ab838a3e25c46f2
SanaTK1 <sana.hasheminia@gmail.com>
2025-09-08 12:44:34 -0400

    Fixed method scope issue in reduce

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -5,7 +5,7 @@ public class Reduce {
         System.out.println(reduce(100));
     }
 
-    private static int reduce(int n) {
+    public static int reduce(int n) {
         int counter = 0;
         while (n > 0){
             if(n%2 == 0)
