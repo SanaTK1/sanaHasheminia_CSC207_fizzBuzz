798ac31e2ebc7a4889dfa0a200356848b23356e1
SanaTK1 <sana.hasheminia@gmail.com>
2025-09-08 11:55:46 -0400

    Added new class Main

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -8,13 +8,13 @@ class FizzBuzz {
     public static void main(String[] args) {
 
         //for (int i = 1; i < 100; i++) {
-        int i = 0;
+        int i = 1;
         while (i < 100){
-            i = fizzBuzzCalculator(i);
+            i = fizzBuzzer(i);
         }
     }
 
-    private static int fizzBuzzCalculator(int i) {
+    public static int fizzBuzzer(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
---
+++ src/lab1/Main.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.fizzBuzzer(5);
+        FizzBuzz.main();
+    }
+}
+
