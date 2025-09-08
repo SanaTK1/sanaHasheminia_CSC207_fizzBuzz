9865f3a8598e380b18d715ce27316f8f70ec821c
Shashemi <135486939+SanaTK1@users.noreply.github.com>
2025-09-08 11:58:42 -0400

    Added a print statment to class Main

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -3,6 +3,7 @@ package lab1;
 public class Main {
     public static void main(String[] args) {
         FizzBuzz.fizzBuzzer(5);
+        System.out.println("Hello world");
         FizzBuzz.main();
     }
 }
